a8cd0918974942051b1edcd4efc14062513a05b9
Artem1209 <72620976+Artem1209@users.noreply.github.com>
2020-10-10 18:57:30 +0300

    Add files via upload

--- Step_Project(Ham)/main.js
+++ Step_Project(Ham)/main.js
@@ -28,7 +28,7 @@ const portfolioHover = {
     web: "<div class='hover'><a href='' class ='hover-link'><img src='img/icon.png' class = 'icon'><h2 class = 'hover-title'>creative design</h2><p class = 'hover-text'>Web Design</p></a></div>",
     wordpress: "<div class='hover'><a href='' class ='hover-link'><img src='img/icon.png' class = 'icon'><h2 class = 'hover-title'>creative design</h2><p class = 'hover-text'>Web Design</p></a></div>",
     landing: "<div class='hover'><a href='' class ='hover-link'><img src='img/icon.png' class = 'icon'><h2 class = 'hover-title'>creative design</h2><p class = 'hover-text'>Web Design</p></a></div>",
-    design: "<div class='hover'><a href='' class ='hover-link'><img src='img/icon.png' class = 'icon'><h2 class = 'hover-title'>creative design</h2><p class = 'hover-text'>Web Design</p></a></div>",
+    graphic: "<div class='hover'><a href='' class ='hover-link'><img src='img/icon.png' class = 'icon'><h2 class = 'hover-title'>creative design</h2><p class = 'hover-text'>Web Design</p></a></div>",
 }
 
 
@@ -41,54 +41,55 @@ loadMoreButton.addEventListener("click",(evt)=>{
     loadMoreButton.style.display = "none";
     for(let i=5;i<=7;i++){
 
-        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items"  >${portfolioHover.web}<img src="filter/graphic-design/graphic-design${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
+        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat= 'graphic'  >${portfolioHover.graphic}<img src="filter/graphic-design/graphic-design${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
 
-        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items"  >${portfolioHover.wordpress}<img src="filter/landing-page/landing-page${i}.jpg" alt=""  class="amazing-img" width="284"  height="220"></li>`);
+        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'landing'  >${portfolioHover.landing}<img src="filter/landing-page/landing-page${i}.jpg" alt=""  class="amazing-img" width="284"  height="220"></li>`);
 
-        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" >${portfolioHover.landing}<img src="filter/web-design/web-design${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
+        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'web' >${portfolioHover.web}<img src="filter/web-design/web-design${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
 
-        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" >${portfolioHover.design}<img src="filter/wordpress/wordpress${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
+        container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'wordpress' >${portfolioHover.wordpress}<img src="filter/wordpress/wordpress${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
     }
 });
 
 for(let i=1;i<=4;i++){
-    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items"  >${portfolioHover.web}<img src="filter/graphic-design/graphic-design${i}.jpg" alt=""  class="amazing-img" width="284"  height="220"></li>`);
-}
-for(let i =1;i<4;i++){
-    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" >${portfolioHover.landing}<img src="filter/landing-page/landing-page${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
-}
-for(let i =1;i<4;i++){
-    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items"  >${portfolioHover.design}<img src="filter/web-design/web-design${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
+    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'graphic'  >${portfolioHover.graphic}<img src="filter/graphic-design/graphic-design${i}.jpg" alt=""  class="amazing-img" width="284"  height="220"></li>`);
 
-}
-for(let i =1;i<4;i++){
-    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" >${portfolioHover.wordpress}<img src="filter/wordpress/wordpress${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
+    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'landing'  >${portfolioHover.landing}<img src="filter/landing-page/landing-page${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
 
+    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'web'  >${portfolioHover.web}<img src="filter/web-design/web-design${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
+
+
+    container.insertAdjacentHTML("afterbegin",`<li class="our-amazing-block-items" data-cat = 'wordpress' >${portfolioHover.wordpress}<img src="filter/wordpress/wordpress${i}.jpg" alt=""  class="amazing-img"  width="284"  height="220"></li>`);
 }
+
 
 
 }
 
-        const filterBox = document.querySelectorAll(".our-amazing-block-items");
-            document.querySelector(".our-amazing-tabs-block").addEventListener("click", e => {
-            if(e.target.tagName !== "LI"){
-                return false;
-            }
-            let filterClass = e.target.dataset['q'];
-            console.log(filterClass);
+$(function filterCategories(){
+    let filter = $("[data-q]");
+    filter.on("click",function(){
+
+        let currentCat = $(".our-amazing-tabs");
+        // currentCat.each(function(){
            
-            filterBox.forEach(element=>{
-                element.classList.remove("hide");
-                if(!element.classList.contains(filterClass) && filterClass !== 'all'){
-                   console.log(1);
-                   element.classList.add("hide");
-                           
+        //    .css("color","#18CFAB");
+        // });
+        let cat = $(this).data("q");
+        if(cat == "all"){
+            $("[data-cat]").removeClass("hide");
+        }
+        else{
+            $("[data-cat]").each(function(){
+                let workCat = $(this).data("cat");
+                if(workCat != cat){
+                    $(this).addClass("hide");
+                }
+                else{
+                    $(this).removeClass("hide");
                 }
             });
-        
-            
-        });
-    
-
-
-
+        }
+       
+    });
+});
